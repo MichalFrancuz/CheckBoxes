79de149863aeb04eb6f106048882831bf5387459
Michal <sasxcxc@icloud.com>
2023-01-21 23:48:55 +0100

    Clone my repository from GitHub. Transfer from Windows to Mac. Problem with JavaFX library.

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -12,6 +12,7 @@ import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 //problem with javafx library
+// still thinking
 
 public class Main extends Application implements EventHandler {
 
